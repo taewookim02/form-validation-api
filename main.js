002221e99ca0590b5c040954cee1a0bffd2821ae
Tae Woo Kim <taewookim02@gmail.com>
2024-06-03 18:49:06 +0900

    feat: Add zip and password validation

--- main.js
+++ main.js
@@ -1,10 +1,63 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // querySelectors
   const form = document.querySelector(".form");
-  // email
   const email = document.querySelector("#email");
   const emailMsg = document.querySelector(".email-error");
+  const country = document.querySelector("#country");
+  const countryMsg = document.querySelector(".country-error");
+  const zipcode = document.querySelector("#zipcode");
+  const zipcodeMsg = document.querySelector(".zipcode-error");
+  const password = document.querySelector("#password");
+  const passwordMsg = document.querySelector(".password-error");
 
   // validations
+  const validateZip = () => {
+    const constraints = {
+      ch: [
+        "^(CH-)?\\d{4}$",
+        "Switzerland ZIPs must have exactly 4 digits: e.g. CH-1950 or 1950",
+      ],
+      fr: [
+        "^(F-)?\\d{5}$",
+        "France ZIPs must have exactly 5 digits: e.g. F-75012 or 75012",
+      ],
+      de: [
+        "^(D-)?\\d{5}$",
+        "Germany ZIPs must have exactly 5 digits: e.g. D-12345",
+      ],
+      nl: [
+        "^(NL-?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
+        "Netherland ZIPs must have exactly 4 digits, followed by 2 letters except SA, SD and SS",
+      ],
+      uk: [
+        "^([A-Z]{1,2}\\d[A-Z\\d]? \\d[A-Z]{2}|GIR 0AA)$",
+        "UK ZIPs must be in one of the following formats: AA9A 9AA, A9A 9AA",
+      ],
+    };
+    // read country id
+    const countryVal = country.value;
+
+    const constraint = new RegExp(constraints[countryVal][0], "");
+
+    if (!constraint.test(zipcode.value)) {
+      // zipcodeMsg.textContent = constraints[countryVal][1];
+      zipcode.setCustomValidity(constraints[countryVal][1]);
+    } else {
+      // zipcodeMsg.textContent = "";
+      zipcode.setCustomValidity("");
+    }
+  };
+
+  const validateCountry = () => {
+    if (country.validity.valueMissing) {
+      countryMsg.textContent = "Country selection is required";
+      country.setCustomValidity("Country is required");
+    } else {
+      countryMsg.textContent = "";
+      country.setCustomValidity("");
+    }
+  };
+
   const validateEmail = () => {
     if (email.validity.typeMismatch) {
       emailMsg.textContent = "Value not in email format";
@@ -12,20 +65,61 @@ document.addEventListener("DOMContentLoaded", () => {
     } else if (email.validity.valueMissing) {
       emailMsg.textContent = "Email is required.";
       email.setCustomValidity("Email is required.");
-    } else if (email.validity.valid) {
+    } else if (email.validity.tooShort) {
+      emailMsg.textContent = "Email too short.";
+      email.setCustomValidity("Email too short.");
+    } else if (email.validity.tooLong) {
+      emailMsg.textContent = "Email too Long.";
+      email.setCustomValidity("Email too Long.");
+    } else {
       emailMsg.textContent = "";
       email.setCustomValidity("");
     }
   };
 
-  // email inline
-  email.addEventListener("focusout", validateEmail);
+  const validatePassword = () => {
+    const passwordRegex = "^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{8,}$";
+    const constraint = new RegExp(passwordRegex);
+    if (!constraint.test(password.value)) {
+      // passwordMsg.textContent =
+      //   "Min 8 characters, at least 1 letter and 1 number";
+      password.setCustomValidity(
+        "Min 8 characters, at least 1 letter and 1 number"
+      );
+    } else {
+      // passwordMsg.textContent = "";
+      password.setCustomValidity("");
+    }
+  };
+
+  const checkValidityOfInputs = () => {
+    return (
+      !email.validity.valid ||
+      !country.validity.valid ||
+      !zipcode.validity.valid ||
+      !password.validity.valid
+    );
+  };
+
+  // eventListeners
+  email.addEventListener("input", validateEmail);
+  country.addEventListener("change", validateCountry);
+  zipcode.addEventListener("focusout", validateZip);
+  password.addEventListener("focusout", validatePassword);
 
   form.addEventListener("submit", (e) => {
-    if (!email.validity.valid) {
+    validateEmail();
+    validateCountry();
+    validateZip();
+    validatePassword();
+
+    if (checkValidityOfInputs()) {
       e.preventDefault();
-      validateEmail();
+
       email.reportValidity();
+      country.reportValidity();
+      zipcode.reportValidity();
+      password.reportValidity();
     }
   });
 });
